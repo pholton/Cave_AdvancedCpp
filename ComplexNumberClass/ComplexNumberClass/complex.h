f7074af903a524d0d467b52bbdb6731e7c2bddcd
Peter <the.peter.holton@gmail.com>
2016-09-28 00:32:44 -0600

    5-25 - 5-27 Operator overloading - the complex number class

--- ComplexNumberClass/ComplexNumberClass/complex.h
+++ ComplexNumberClass/ComplexNumberClass/complex.h
@@ -14,7 +14,7 @@ namespace cop {
 		// Constructors and Destructors
 		Complex();
 		Complex(double real, double imaginary);
-		Complex(const Complex &other);  // Copy constructor
+		Complex(const Complex &other);
 		~Complex() {}
 
 		// Getters
